1a83cd5d60472512092caa39084e896b6480cd32
Sam Kim <samkim@Sams-MacBook-Pro.local>
2020-01-09 13:41:25 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Header />
+      <Body />
+      <Footer />
     </div>
   )
 }
