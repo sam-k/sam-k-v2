384feaae48c14112f04e894eb7f8338526801722
Sam Kim <sam.kim@duke.edu>
2020-01-17 10:12:56 -0500

    Implementing ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,14 +4,20 @@ class ProjectCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            title: props.title,
+            subtitle: props.subtitle,
+            text: props.text,
+            tools: props.tools,
+            links: props.links
         }
     }
 
     render() {
         return (
-            <div className='projectCard'>
-                <p>Test</p>
+            <div className='project-card'>
+                <h3>{this.state.title}</h3>
+                <h4>{this.state.subtitle}</h4>
+                <p>{this.state.text}</p>
             </div>
         )
     }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,11 @@ import ProjectCard from "./ProjectCard"
 class Projects extends React.Component {
     render() {
         return (
-            <div id='project-wrapper' >
-                <ProjectCard />
+            <div id='project-wrapper'>
+                <ProjectCard
+                    title="Handoff, a web app"
+                    subtitle="Best nonprofit hack co-winner at HackDuke 2019"
+                    text="Serving a local volunteer group’s ridesharing needs for developmentally disabled adults. Includes " />
                 <ProjectCard />
                 <ProjectCard />
             </div>
