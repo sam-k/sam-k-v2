1862ba30fdca03249653971bca3c77f9b32a5aef
Sam Kim <sam.kim@duke.edu>
2020-01-27 22:12:33 -0500

    Start Work

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,22 +1,9 @@
 import React from "react"
 import Skills from "./Skills"
+import Work from "./Work"
 
 class Experience extends React.Component {
     render() {
-
-        const work = {
-            "Epic Systems":
-                "https://www.epic.com/",
-            "Duke Center for Genomic and Computational Biology":
-                "https://genome.duke.edu/",
-            "National Security Innovation Network":
-                "https://www.nsin.us/",
-            "National Institutes of Health":
-                "https://www.nih.gov/",
-            "Duke University School of Medicine":
-                "https://medschool.duke.edu/"
-        }
-
         return (
             <div id='experience-wrapper'>
                 <div id='skills-wrapper'>
@@ -25,8 +12,9 @@ class Experience extends React.Component {
                 </div>
                 <div id='work-wrapper'>
                     <h2>Work</h2>
+                    <Work />
                 </div>
-            </div >
+            </div>
         )
     }
 }
