a5268efa6df8e8f6b148481fb6325a0807c54804
Sam Kim <samkim1729@gmail.com>
2020-03-12 01:57:11 -0400

    Switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter, Route, Switch } from "react-router-dom"
 import ScrollToTop from "./ScrollToTop"
 // import { createBrowserHistory } from "history"
 
@@ -12,18 +12,18 @@ import Footer from "./components/Footer"
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <HashRouter basename="/">
         <ScrollToTop />
         <div id='App'>
           <Navbar />
           <Switch>
             <Route path="/projects" component={ProjectsPage} />
             <Route path="/about" component={AboutPage} />
-            <Route path="/" component={HomePage} />
+            <Route exact path="/" component={HomePage} />
           </Switch>
           <Footer />
         </div>
-      </Router >
+      </HashRouter >
     )
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -54,20 +54,18 @@ class Button extends React.Component {
         let buttonElement = null
         if (this.state.linkType === 'router') {
             buttonElement =
-                <Link to={this.state.link}>
-                    <div
-                        style={buttonProps.style}
-                        onMouseEnter={buttonProps.onMouseEnter}
-                        onMouseLeave={buttonProps.onMouseLeave}
-                    >
-                        {this.state.icon &&
-                            <i
-                                className={this.state.icon}
-                                style={{ color: this.state.iconColor }}
-                            />
-                        }
-                        <span>{this.state.text}</span>
-                    </div>
+                <Link to={this.state.link}
+                    style={buttonProps.style}
+                    onMouseEnter={buttonProps.onMouseEnter}
+                    onMouseLeave={buttonProps.onMouseLeave}
+                >
+                    {this.state.icon &&
+                        <i
+                            className={this.state.icon}
+                            style={{ color: this.state.iconColor }}
+                        />
+                    }
+                    <span>{this.state.text}</span>
                 </Link>
         } else {
             buttonElement =
