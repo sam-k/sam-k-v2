c7a8d66ab8a7d409873893637069c957ec155ebc
Sam Kim <samkim1729@gmail.com>
2020-03-12 01:03:20 -0400

    Minor change ♥

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,7 +30,7 @@ class Footer extends React.Component {
                                 </li>
                             </ul>
                             <ul id='copyright-lines'>
-                                <li>Designed and built by </li>
+                                <li>Designed and built with <span id='heart'>♥</span> by </li>
                                 <li>Sam Kim, © 2020</li>
                             </ul>
                         </div>
