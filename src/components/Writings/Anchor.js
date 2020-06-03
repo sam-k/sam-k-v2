a47c2b52593ad04ff06f445161d68188a5386799
Sam Kim <samkim1729@gmail.com>
2020-06-03 18:58:23 -0500

    Allow calling custom functions

--- src/components/Writings/Anchor.js
+++ src/components/Writings/Anchor.js
@@ -2,12 +2,23 @@ import React from "react"
 
 // Generic anchor component.
 class Anchor extends React.Component {
+    constructor(props) {
+        super(props)
+        this.thisFunction = this.thisFunction.bind(this)
+    }
+
+    thisFunction() {
+        this.props.function()
+        document.getElementById("anchor" + this.props.n).scrollIntoView({ behavior: "smooth" })
+    }
+
     render() {
         return (
             <span className='anchor' onClick={
-                () => document.getElementById("anchor" + this.props.n).scrollIntoView({
-                    behavior: "smooth"
-                })
+                () => {
+                    this.props.function()
+                    document.getElementById("anchor" + this.props.n).scrollIntoView({ behavior: "smooth" })
+                }
             }>
                 {this.props.text}
             </span>
